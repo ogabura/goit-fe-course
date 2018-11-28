6ab716ab70db26c53f8a271de0f3f4d9dfd4721e
Alexsander Gabura <40072572+ogabura@users.noreply.github.com>
2018-11-29 00:12:22 +0200

    comment

--- js/module-1/scripts/base.js
+++ js/module-1/scripts/base.js
@@ -15,17 +15,17 @@ const welcomeMessage = 'Добро пожаловать, ' + login + '!';
 const cancelMessage = 'Отменено пользователем!';
 
 if (login === cancel ) {
-    alert (cancelMessage);
+    alert (cancelMessage); /* сообщение про отмену юзером в случае нажатии на кнопу отмены в поле логина */
   } else 
     if (login === adminLogin) { 
       const pass = prompt (' Введите пароль:'); /* ввод пароля только если логин совпал */
       if (pass === cancel ) {
-        alert (cancelMessage);
+        alert (cancelMessage); /* сообщение про отмену юзером в случае нажатии на кнопу отмены в поле пароля */
         } else 
           if (pass === adminPassword) {
             alert (welcomeMessage);
           } else {
-            alert (badPassMessage);
+            alert (badPassMessage); 
           }
       } else {
         alert (badLoginMessage);
