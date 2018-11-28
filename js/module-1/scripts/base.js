7b2d25e5af2b5a86a10c9cbbfc7398db261d0f7e
Alexsander Gabura <40072572+ogabura@users.noreply.github.com>
2018-11-28 23:44:40 +0200

    added

--- js/module-1/scripts/base.js
+++ js/module-1/scripts/base.js
@@ -3,21 +3,28 @@
 const adminLogin = 'admin';
 /* const adminPassword = 'm4ng0h4ckz'; */
 const adminPassword = '1';
+const cancel = null;
 
-const login = prompt (' Введите логин:');
+let login = prompt (' Введите логин:');
 
+/* const messages */
 
 const badLoginMessage = 'Доступ запрещен, неверный логин!';
 const badPassMessage = 'Доступ запрещен, неверный пароль!';
 const welcomeMessage = 'Добро пожаловать, ' + login + '!';
+const cancelMessage = 'Отменено пользователем!';
 
-if (login === adminLogin) { 
-  const pass = prompt (' Введите пароль:'); /* ввод пароля только если логин совпал */
-  if (pass === adminPassword) {
-    alert (welcomeMessage);
-  } else {
-    alert (badPassMessage);
-  }
-} else {
-  alert (badLoginMessage);
-};
+if (login === cancel ) {
+    alert (cancelMessage);
+  } else 
+    if (login === adminLogin) { 
+      const pass = prompt (' Введите пароль:'); /* ввод пароля только если логин совпал */
+      if (pass === adminPassword) {
+        alert (welcomeMessage);
+      } else {
+        alert (badPassMessage);
+      }
+    } else {
+      alert (badLoginMessage);
+    }
+  ;
